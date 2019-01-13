20bbe26561c27de44b66667d41100e97a3df4ca8
John Worley <john.aver.worley@gmail.com>
2019-01-12 20:26:33 -0600

    all dom logic moved to single piece reducer

--- src/components/SingleResultPage/singleResultPage.jsx
+++ src/components/SingleResultPage/singleResultPage.jsx
@@ -10,40 +10,30 @@ const mapStateToProps = reduxStore => {
 
 class SingleResultPage extends Component {
     componentDidUpdate() {
-        console.log(`metadata`, this.props.piece.metadata[4]);
+    
+        console.log(this.props.piece.technique);
         
 
     }
     render() {
 
+    
         
-        if (this.props.piece.metadata) {
-            var date = this.props.piece.metadata.filter(function (el) {
-                return el.label === 'Date';
-            });
-        }
-
-        if (this.props.piece.metadata) {
-            var classification = this.props.piece.metadata.filter(function (el) {
-                return el.label === 'Classification';
-            });
-        }
-        
-        if (this.props.piece.metadata) {
-            var people = this.props.piece.metadata.filter(function (el) {
-                return el.label === 'Classification';
-            });
-        }
-      
-        console.log(date);
         
         return (
             <div>
                 
                 <h1>{this.props.piece.title}</h1>
                 <img src={this.props.piece.imageUrl} alt=""></img>
-                {date && <p>{date[0].value}</p>}
-                {classification && <p>{classification[0].value}</p>}
+                <p>{this.props.piece.date}</p>
+                <p>{this.props.piece.classification}</p>
+                <p>{this.props.piece.people}</p>
+                <p>{this.props.piece.medium}</p>
+                <p>{this.props.piece.dimensions}</p>
+                <p>{this.props.piece.technique}</p>
+              
+
+
             </div>
         )
 
--- src/redux/reducers/singlePieceReducer.js
+++ src/redux/reducers/singlePieceReducer.js
@@ -6,20 +6,79 @@ const piece = (state = {}, action) => {
     const defaultState = {
         imageUrl: "",
         title: "",
-        metadata: {}
+        medium: "",
+        dimensions: "",
+        date: "",
+        classification: "",
+        people:"",
+        technique: "",
+
     }
-    
+
     switch (action.type) {
         case 'SET_PIECE':
-            if (action.payload.sequences && action.payload.sequences[0].canvases[0]){
-            defaultState.imageUrl = action.payload.sequences[0].canvases[0].images[0].resource["@id"]
+            if (action.payload.sequences && action.payload.sequences[0].canvases[0]) {
+                defaultState.imageUrl = action.payload.sequences[0].canvases[0].images[0].resource["@id"]
             } else {
                 defaultState.imageUrl = ""
             }
+            if (action.payload.metadata) {
+                var medium = action.payload.metadata.filter(function (el) {
+                    return el.label === 'Medium';
+                });
+                defaultState.medium = medium[0].value;
+            } else {
+                defaultState.medium = "";
+            }
 
-            defaultState.title = action.payload.label ;
-            defaultState.metadata = action.payload.metadata ;
-           
+            if (action.payload.metadata) {
+                var dimensions = action.payload.metadata.filter(function (el) {
+                    return el.label === 'Dimensions';
+                });
+                defaultState.dimensions = dimensions[0].value;
+            } else {
+                return defaultState.dimensions = "";
+            }
+
+            if (action.payload.metadata) {
+                var technique = action.payload.metadata.filter(function (el) {
+                    return el.label === 'Technique';
+                });
+                defaultState.technique = technique.value;
+            } else {
+                return defaultState.technique = "";
+            }
+
+            if (action.payload.metadata) {
+                var date = action.payload.metadata.filter(function (el) {
+                    return el.label === 'Date';
+                });
+                defaultState.date = date[0].value;
+            } else {
+                return defaultState.date = "";
+            }
+
+            if (action.payload.metadata) {
+                var classification = action.payload.metadata.filter(function (el) {
+                    return el.label === 'Classification';
+                });
+                defaultState.classification = classification[0].value;
+            } else {
+                return defaultState.classification = "";
+            }
+
+            if (action.payload.metadata) {
+                var people = action.payload.metadata.filter(function (el) {
+                    return el.label === 'People';
+                });
+                defaultState.people = people[0].value[0];
+            } else {
+                return defaultState.people = "";
+            }
+
+            defaultState.title = action.payload.label;
+
+
             return defaultState;
         default:
             return state;
